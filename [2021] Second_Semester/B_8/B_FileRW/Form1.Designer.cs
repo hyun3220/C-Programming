10da34a911df40c5e887249ae9736878d1c5d973
hyun3220 <ahdtlf3220@naver.com>
2021-10-19 16:41:18 +0900

    21/10/19 8주차 File I/O 구현

---
+++ [2021] Second_Semester/B_8/B_FileRW/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace B_FileRW
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabMenu = new System.Windows.Forms.TabControl();
+            this.tpRead = new System.Windows.Forms.TabPage();
+            this.txtRView = new System.Windows.Forms.TextBox();
+            this.txtRPath = new System.Windows.Forms.TextBox();
+            this.btnRLRead = new System.Windows.Forms.Button();
+            this.btnRARead = new System.Windows.Forms.Button();
+            this.btnRPath = new System.Windows.Forms.Button();
+            this.tpWrite = new System.Windows.Forms.TabPage();
+            this.txtWView = new System.Windows.Forms.TextBox();
+            this.txtWPath = new System.Windows.Forms.TextBox();
+            this.btnWLSave = new System.Windows.Forms.Button();
+            this.btnWASave = new System.Windows.Forms.Button();
+            this.btnWPath = new System.Windows.Forms.Button();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
+            this.tabMenu.SuspendLayout();
+            this.tpRead.SuspendLayout();
+            this.tpWrite.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabMenu
+            // 
+            this.tabMenu.Controls.Add(this.tpRead);
+            this.tabMenu.Controls.Add(this.tpWrite);
+            this.tabMenu.Location = new System.Drawing.Point(0, 0);
+            this.tabMenu.Name = "tabMenu";
+            this.tabMenu.SelectedIndex = 0;
+            this.tabMenu.Size = new System.Drawing.Size(463, 343);
+            this.tabMenu.TabIndex = 0;
+            // 
+            // tpRead
+            // 
+            this.tpRead.Controls.Add(this.txtRView);
+            this.tpRead.Controls.Add(this.txtRPath);
+            this.tpRead.Controls.Add(this.btnRLRead);
+            this.tpRead.Controls.Add(this.btnRARead);
+            this.tpRead.Controls.Add(this.btnRPath);
+            this.tpRead.Location = new System.Drawing.Point(4, 24);
+            this.tpRead.Name = "tpRead";
+            this.tpRead.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRead.Size = new System.Drawing.Size(455, 315);
+            this.tpRead.TabIndex = 0;
+            this.tpRead.Text = "파일 읽기";
+            this.tpRead.UseVisualStyleBackColor = true;
+            // 
+            // txtRView
+            // 
+            this.txtRView.Location = new System.Drawing.Point(8, 109);
+            this.txtRView.Multiline = true;
+            this.txtRView.Name = "txtRView";
+            this.txtRView.ReadOnly = true;
+            this.txtRView.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtRView.Size = new System.Drawing.Size(434, 193);
+            this.txtRView.TabIndex = 4;
+            // 
+            // txtRPath
+            // 
+            this.txtRPath.Location = new System.Drawing.Point(8, 27);
+            this.txtRPath.Name = "txtRPath";
+            this.txtRPath.ReadOnly = true;
+            this.txtRPath.Size = new System.Drawing.Size(355, 23);
+            this.txtRPath.TabIndex = 3;
+            // 
+            // btnRLRead
+            // 
+            this.btnRLRead.Location = new System.Drawing.Point(89, 70);
+            this.btnRLRead.Name = "btnRLRead";
+            this.btnRLRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRLRead.TabIndex = 2;
+            this.btnRLRead.Text = "라인 읽기";
+            this.btnRLRead.UseVisualStyleBackColor = true;
+            this.btnRLRead.Click += new System.EventHandler(this.btnRLRead_Click);
+            // 
+            // btnRARead
+            // 
+            this.btnRARead.Location = new System.Drawing.Point(8, 70);
+            this.btnRARead.Name = "btnRARead";
+            this.btnRARead.Size = new System.Drawing.Size(75, 23);
+            this.btnRARead.TabIndex = 1;
+            this.btnRARead.Text = "전체 읽기";
+            this.btnRARead.UseVisualStyleBackColor = true;
+            this.btnRARead.Click += new System.EventHandler(this.btnRARead_Click);
+            // 
+            // btnRPath
+            // 
+            this.btnRPath.Location = new System.Drawing.Point(374, 27);
+            this.btnRPath.Name = "btnRPath";
+            this.btnRPath.Size = new System.Drawing.Size(75, 23);
+            this.btnRPath.TabIndex = 0;
+            this.btnRPath.Text = "불러오기";
+            this.btnRPath.UseVisualStyleBackColor = true;
+            this.btnRPath.Click += new System.EventHandler(this.btnRPath_Click);
+            // 
+            // tpWrite
+            // 
+            this.tpWrite.Controls.Add(this.txtWView);
+            this.tpWrite.Controls.Add(this.txtWPath);
+            this.tpWrite.Controls.Add(this.btnWLSave);
+            this.tpWrite.Controls.Add(this.btnWASave);
+            this.tpWrite.Controls.Add(this.btnWPath);
+            this.tpWrite.Location = new System.Drawing.Point(4, 24);
+            this.tpWrite.Name = "tpWrite";
+            this.tpWrite.Padding = new System.Windows.Forms.Padding(3);
+            this.tpWrite.Size = new System.Drawing.Size(455, 315);
+            this.tpWrite.TabIndex = 1;
+            this.tpWrite.Text = "파일 쓰기";
+            this.tpWrite.UseVisualStyleBackColor = true;
+            // 
+            // txtWView
+            // 
+            this.txtWView.Location = new System.Drawing.Point(8, 109);
+            this.txtWView.Multiline = true;
+            this.txtWView.Name = "txtWView";
+            this.txtWView.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtWView.Size = new System.Drawing.Size(434, 192);
+            this.txtWView.TabIndex = 4;
+            // 
+            // txtWPath
+            // 
+            this.txtWPath.Location = new System.Drawing.Point(8, 27);
+            this.txtWPath.Name = "txtWPath";
+            this.txtWPath.ReadOnly = true;
+            this.txtWPath.Size = new System.Drawing.Size(355, 23);
+            this.txtWPath.TabIndex = 3;
+            // 
+            // btnWLSave
+            // 
+            this.btnWLSave.Location = new System.Drawing.Point(89, 70);
+            this.btnWLSave.Name = "btnWLSave";
+            this.btnWLSave.Size = new System.Drawing.Size(75, 23);
+            this.btnWLSave.TabIndex = 2;
+            this.btnWLSave.Text = "라인 저장";
+            this.btnWLSave.UseVisualStyleBackColor = true;
+            this.btnWLSave.Click += new System.EventHandler(this.btnWLSave_Click);
+            // 
+            // btnWASave
+            // 
+            this.btnWASave.Location = new System.Drawing.Point(8, 70);
+            this.btnWASave.Name = "btnWASave";
+            this.btnWASave.Size = new System.Drawing.Size(75, 23);
+            this.btnWASave.TabIndex = 1;
+            this.btnWASave.Text = "전체 저장";
+            this.btnWASave.UseVisualStyleBackColor = true;
+            this.btnWASave.Click += new System.EventHandler(this.btnWASave_Click);
+            // 
+            // btnWPath
+            // 
+            this.btnWPath.Location = new System.Drawing.Point(374, 27);
+            this.btnWPath.Name = "btnWPath";
+            this.btnWPath.Size = new System.Drawing.Size(75, 23);
+            this.btnWPath.TabIndex = 0;
+            this.btnWPath.Text = "저장하기";
+            this.btnWPath.UseVisualStyleBackColor = true;
+            this.btnWPath.Click += new System.EventHandler(this.btnWPath_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Filter = "텍스트 파일(*.txt)|*.txt|모든 파일(*.*)|*.*";
+            // 
+            // sfdFile
+            // 
+            this.sfdFile.Filter = "텍스트 파일(*.txt)|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 338);
+            this.Controls.Add(this.tabMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "문자 입출력";
+            this.tabMenu.ResumeLayout(false);
+            this.tpRead.ResumeLayout(false);
+            this.tpRead.PerformLayout();
+            this.tpWrite.ResumeLayout(false);
+            this.tpWrite.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabMenu;
+        private System.Windows.Forms.TabPage tpRead;
+        private System.Windows.Forms.TabPage tpWrite;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.SaveFileDialog sfdFile;
+        private System.Windows.Forms.TextBox txtRView;
+        private System.Windows.Forms.TextBox txtRPath;
+        private System.Windows.Forms.Button btnRLRead;
+        private System.Windows.Forms.Button btnRARead;
+        private System.Windows.Forms.Button btnRPath;
+        private System.Windows.Forms.TextBox txtWView;
+        private System.Windows.Forms.TextBox txtWPath;
+        private System.Windows.Forms.Button btnWLSave;
+        private System.Windows.Forms.Button btnWASave;
+        private System.Windows.Forms.Button btnWPath;
+    }
+}
+
---
+++ [2021] Second_Semester/B_8/B_FileRW/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;  
+/* [System.IO 네임스페이스]
+   파일과 데이터 스트림을 읽고 쓸 수 있게 하는 형식, 
+   기본 파일과 디렉터리 지원을 제공하는 인터페이스를 지원한다. */
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_FileRW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRPath_Click(object sender, EventArgs e)
+        {
+            if(this.ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                this.txtRPath.Text = this.ofdFile.FileName;
+            }
+        }
+
+        private void btnRARead_Click(object sender, EventArgs e)
+        {
+            if(txtCheck() == false)
+            {
+                return;
+            }
+            if(File.Exists(this.txtRPath.Text))
+            {
+                /* 파일을 읽어들이는 과정, 파일 입출력을 위한 StreamReader 생성자 생성 */
+                /* new StreamReader(파일의 경로, 인코딩 방식) 형식으로 작성한다. */
+                /* sr객체에 메모리를 할당 */
+                using (StreamReader sr = new StreamReader(this.txtRPath.Text, Encoding.Default))
+                {
+                    this.txtRView.Text = sr.ReadToEnd();    // 선택한 txt파일의 내용을 출력
+                }
+            }
+            else
+            {
+                MessageBox.Show("읽을 파일이 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public bool txtCheck()
+        {
+            if(this.txtRPath.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void btnRLRead_Click(object sender, EventArgs e)
+        {
+            if (txtCheck() == false)
+            {
+                return;
+            }
+            this.txtRView.Clear();
+
+            if (File.Exists(this.txtRPath.Text))
+            {
+                using (StreamReader sr = new StreamReader(this.txtRPath.Text, Encoding.Default))
+                {
+                    string line = null;
+
+                    while((line = sr.ReadLine()) != null)   // 인덱스 번호를 반복해 읽어들이며 출력
+                    {
+                        this.txtRView.AppendText(line + "\r\n");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("읽을 파일이 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWPath_Click(object sender, EventArgs e)
+        {
+            if(this.sfdFile.ShowDialog() == DialogResult.OK)
+            {
+                this.txtWPath.Text = this.sfdFile.FileName;
+            }
+        }
+
+        private void btnWASave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(this.txtWPath.Text))
+                {
+                    sw.WriteLine(this.txtWView.Text);
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            MessageBox.Show("파일이 저장되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnWLSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(this.txtWPath.Text))
+                {
+                    foreach (var str in this.txtWView.Lines)    // 입력한 데이터를 한 행씩 읽어서 저장
+                    {
+                        sw.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+            MessageBox.Show("파일이 저장되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}

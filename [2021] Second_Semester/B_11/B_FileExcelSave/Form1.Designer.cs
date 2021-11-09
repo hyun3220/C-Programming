db6f667d4118fb63292f13266ecb6f0efb9f0019
hyun3220 <ahdtlf3220@naver.com>
2021-11-09 16:41:27 +0900

    21/11/09 11주차 - 파일 정보 출력, 저장(txt, xlsx)

---
+++ [2021] Second_Semester/B_11/B_FileExcelSave/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace B_FileExcelSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.gbSave = new System.Windows.Forms.GroupBox();
+            this.rbExcel = new System.Windows.Forms.RadioButton();
+            this.rbText = new System.Windows.Forms.RadioButton();
+            this.lvFile = new System.Windows.Forms.ListView();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chDate = new System.Windows.Forms.ColumnHeader();
+            this.chType = new System.Windows.Forms.ColumnHeader();
+            this.chSize = new System.Windows.Forms.ColumnHeader();
+            this.chPath = new System.Windows.Forms.ColumnHeader();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
+            this.gbSave.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(12, 12);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(75, 59);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "파일";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(299, 32);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // gbSave
+            // 
+            this.gbSave.Controls.Add(this.rbExcel);
+            this.gbSave.Controls.Add(this.rbText);
+            this.gbSave.Location = new System.Drawing.Point(93, 12);
+            this.gbSave.Name = "gbSave";
+            this.gbSave.Size = new System.Drawing.Size(200, 59);
+            this.gbSave.TabIndex = 3;
+            this.gbSave.TabStop = false;
+            this.gbSave.Text = "파일 저장";
+            // 
+            // rbExcel
+            // 
+            this.rbExcel.AutoSize = true;
+            this.rbExcel.Location = new System.Drawing.Point(103, 24);
+            this.rbExcel.Name = "rbExcel";
+            this.rbExcel.Size = new System.Drawing.Size(58, 19);
+            this.rbExcel.TabIndex = 1;
+            this.rbExcel.TabStop = true;
+            this.rbExcel.Text = "EXCEL";
+            this.rbExcel.UseVisualStyleBackColor = true;
+            // 
+            // rbText
+            // 
+            this.rbText.AutoSize = true;
+            this.rbText.Location = new System.Drawing.Point(38, 24);
+            this.rbText.Name = "rbText";
+            this.rbText.Size = new System.Drawing.Size(44, 19);
+            this.rbText.TabIndex = 0;
+            this.rbText.TabStop = true;
+            this.rbText.Text = "TXT";
+            this.rbText.UseVisualStyleBackColor = true;
+            // 
+            // lvFile
+            // 
+            this.lvFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chDate,
+            this.chType,
+            this.chSize,
+            this.chPath});
+            this.lvFile.FullRowSelect = true;
+            this.lvFile.GridLines = true;
+            this.lvFile.HideSelection = false;
+            this.lvFile.Location = new System.Drawing.Point(12, 77);
+            this.lvFile.Name = "lvFile";
+            this.lvFile.Size = new System.Drawing.Size(686, 236);
+            this.lvFile.TabIndex = 4;
+            this.lvFile.UseCompatibleStateImageBehavior = false;
+            this.lvFile.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 150;
+            // 
+            // chDate
+            // 
+            this.chDate.Text = "수정한 날짜";
+            this.chDate.Width = 100;
+            // 
+            // chType
+            // 
+            this.chType.Text = "유형";
+            this.chType.Width = 100;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "크기";
+            this.chSize.Width = 100;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "경로";
+            this.chPath.Width = 250;
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Filter = "모든 파일(*.*)|*.*";
+            this.ofdFile.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 325);
+            this.Controls.Add(this.lvFile);
+            this.Controls.Add(this.gbSave);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPath);
+            this.Name = "Form1";
+            this.Text = "파일 정보 저장";
+            this.gbSave.ResumeLayout(false);
+            this.gbSave.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox gbSave;
+        private System.Windows.Forms.RadioButton rbExcel;
+        private System.Windows.Forms.RadioButton rbText;
+        private System.Windows.Forms.ListView lvFile;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chDate;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.ColumnHeader chSize;
+        private System.Windows.Forms.ColumnHeader chPath;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.SaveFileDialog sfdFile;
+    }
+}
+
---
+++ [2021] Second_Semester/B_11/B_FileExcelSave/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// Excel 파일을 읽고 쓰는데 필요한 클래스, 메소드등의 인터페이스 사용을 쉽게 한다.
+// 네임스페이스의 별칭을 부여해 Excel로 편하게 사용한다.
+using Excel = Microsoft.Office.Interop.Excel;   
+
+
+namespace B_FileExcelSave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* 인스턴스 객체, 변수를 초기화 하지 않으면 쓰레기 값이 나올 여지가 생김*/
+
+        FileInfo fi = null;     // 파일 정보 가져오기 (FileInfo : System.IO(입출력) 관련 클래스)
+        string FsPath = "";     // 파일 경로 저장 (파일 멤버에 변수, 메소드, 속성, 생성자 등 포함)
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if(this.ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string f in this.ofdFile.FileNames)
+                {
+                    fi = new FileInfo(f);
+
+                    this.lvFile.Items.Add(new ListViewItem(new string[] 
+                    { fi.Name, fi.LastWriteTime.ToString(), fi.Extension.Split('.')[1], 
+                        GetFileSize(fi.Length), fi.FullName }));
+                }
+            }
+        }
+
+        private string GetFileSize(double byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount >= 1073741824.0)
+            {
+                // {0:##.##} : 0 = Index 번호 (byteCount / 1073741824.0) 값 해당, ##.## = 표현 형식 지정
+                size = String.Format("{0:##.##}", byteCount / 1073741824.0) + " GB";
+            }
+            else if (byteCount >= 1048576.0)
+            {
+                size = String.Format("{0:##.##}", byteCount / 1048576.0) + " MB";
+            }
+            else if (byteCount >= 1024.0)
+            {
+                size = String.Format("{0:##.##}", byteCount / 1024.0) + " KB";
+            }
+            else if (byteCount > 0 && byteCount < 1024.0)
+            {
+                size = byteCount.ToString() + " Bytes";
+            }
+
+            return size;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(this.lvFile.Items.Count == 0)
+            {
+                MessageBox.Show("저장할 파일 정보가 없습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                return;
+            }
+            if (this.rbText.Checked == true)
+            {
+                this.sfdFile.Filter = "텍스트 파일(*.txt) | *.txt";
+                if (this.sfdFile.ShowDialog() == DialogResult.OK)
+                {
+                    FsPath = this.sfdFile.FileName;
+                    TxtSave();
+                }
+            }
+            else
+            {
+                this.sfdFile.Filter = "엑셀 파일(*.xlsx) | *.xlsx";
+                if (this.sfdFile.ShowDialog() == DialogResult.OK)
+                {
+                    FsPath = this.sfdFile.FileName;
+                    ExcelSave();
+                }
+            }
+        }
+        private void TxtSave()
+        {
+            // using : 메모리를 할당하는 객체 선언, 안정적으로 메모리 할당 가능 using을 벗어날 경우에도 안정적인 메모리 해제가 가능
+            using (StreamWriter sw = new StreamWriter(FsPath))
+            {
+                sw.WriteLine("이름" + "\t" + "수정한 날짜" + "\t" + "유형" + "\t" + "크기" + "\t" + "경로" + "\t");
+
+                for(int n = 0; n < this.lvFile.Items.Count; n++)
+                {
+                    string FInfo = "";
+
+                    for(int i = 0; i < this.lvFile.Items[n].SubItems.Count; i++)
+                    {
+                        FInfo += this.lvFile.Items[n].SubItems[i].Text + "\t";
+                    }
+                    sw.WriteLine(FInfo);
+                }
+                sw.Close();
+            }
+        }
+
+        private void ExcelSave()
+        {
+            Excel.Application eApp;
+            Excel.Workbook eWorkbook;
+            Excel.Worksheet eWorkSheet;
+
+            string[,] data;
+
+            eApp = new Excel.Application();
+            eWorkbook = eApp.Workbooks.Add(true);
+            eWorkSheet = (Excel.Worksheet)eWorkbook.Sheets[1];  // Excel Sheet 배열은 1부터 시작한다.
+
+            int rNum = this.lvFile.Items.Count + 1;
+            int cNum = 5;
+
+            data = new string[rNum, cNum];
+            data[0, 0] = "이름";
+            data[0, 1] = "수정한 날짜";
+            data[0, 2] = "유형";
+            data[0, 3] = "크기";
+            data[0, 4] = "경로";
+
+            for (int n = 0; n < this.lvFile.Items.Count; n++)
+            {
+                for(int i = 0; i < this.lvFile.Items[n].SubItems.Count; i++)
+                {
+                    data[n + 1, i] = this.lvFile.Items[n].SubItems[i].Text;
+                }
+            }
+            string EndStr = "E" + rNum.ToString();
+            eWorkSheet.get_Range("A1 : " + EndStr).Value2 = data;
+            eWorkbook.SaveAs(FsPath, Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Excel.XlSaveAsAccessMode.xlShared, false, false, Type.Missing, Type.Missing, Type.Missing);
+            eWorkbook.Close(false, Type.Missing, Type.Missing);
+            eApp.Quit();
+
+        }
+    }
+}
+

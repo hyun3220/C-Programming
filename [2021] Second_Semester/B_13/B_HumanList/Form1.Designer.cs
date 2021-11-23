ffadc455133d2e9481550f1fea6dbf3ba1a35810
hyun3220 <ahdtlf3220@naver.com>
2021-11-23 16:51:41 +0900

    21/11/23 13주차 - 인명부 관리 프로그램

---
+++ [2021] Second_Semester/B_13/B_HumanList/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace B_HumanList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plGroup = new System.Windows.Forms.Panel();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblJob = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lvList = new System.Windows.Forms.ListView();
+            this.chId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chJob = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.plGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plGroup
+            // 
+            this.plGroup.BackColor = System.Drawing.Color.White;
+            this.plGroup.Controls.Add(this.btnDel);
+            this.plGroup.Controls.Add(this.btnModify);
+            this.plGroup.Controls.Add(this.btnSave);
+            this.plGroup.Controls.Add(this.txtAge);
+            this.plGroup.Controls.Add(this.txtName);
+            this.plGroup.Controls.Add(this.txtPhone);
+            this.plGroup.Controls.Add(this.txtJob);
+            this.plGroup.Controls.Add(this.lblName);
+            this.plGroup.Controls.Add(this.lblAge);
+            this.plGroup.Controls.Add(this.lblJob);
+            this.plGroup.Controls.Add(this.lblPhone);
+            this.plGroup.Location = new System.Drawing.Point(12, 196);
+            this.plGroup.Name = "plGroup";
+            this.plGroup.Size = new System.Drawing.Size(580, 111);
+            this.plGroup.TabIndex = 0;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(478, 63);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 11;
+            this.btnDel.Text = "삭제";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(478, 24);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 10;
+            this.btnModify.Text = "수정";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(376, 17);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(85, 78);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(53, 74);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 21);
+            this.txtAge.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(53, 16);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(252, 17);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 21);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // txtJob
+            // 
+            this.txtJob.Location = new System.Drawing.Point(252, 74);
+            this.txtJob.Name = "txtJob";
+            this.txtJob.Size = new System.Drawing.Size(100, 21);
+            this.txtJob.TabIndex = 5;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("굴림", 9F);
+            this.lblName.Location = new System.Drawing.Point(18, 19);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(29, 12);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "이름";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("굴림", 9F);
+            this.lblAge.Location = new System.Drawing.Point(18, 76);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 12);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "나이";
+            // 
+            // lblJob
+            // 
+            this.lblJob.AutoSize = true;
+            this.lblJob.Font = new System.Drawing.Font("굴림", 9F);
+            this.lblJob.Location = new System.Drawing.Point(207, 77);
+            this.lblJob.Name = "lblJob";
+            this.lblJob.Size = new System.Drawing.Size(29, 12);
+            this.lblJob.TabIndex = 3;
+            this.lblJob.Text = "직업";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("굴림", 9F);
+            this.lblPhone.Location = new System.Drawing.Point(193, 19);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(53, 12);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "전화번호";
+            // 
+            // lvList
+            // 
+            this.lvList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chId,
+            this.chName,
+            this.chAge,
+            this.chPhone,
+            this.chJob});
+            this.lvList.FullRowSelect = true;
+            this.lvList.GridLines = true;
+            this.lvList.HideSelection = false;
+            this.lvList.Location = new System.Drawing.Point(12, 12);
+            this.lvList.Name = "lvList";
+            this.lvList.Size = new System.Drawing.Size(580, 178);
+            this.lvList.TabIndex = 12;
+            this.lvList.UseCompatibleStateImageBehavior = false;
+            this.lvList.View = System.Windows.Forms.View.Details;
+            this.lvList.Click += new System.EventHandler(this.lvList_Click);
+            // 
+            // chId
+            // 
+            this.chId.Text = "구분";
+            this.chId.Width = 50;
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 80;
+            // 
+            // chAge
+            // 
+            this.chAge.Text = "나이";
+            // 
+            // chPhone
+            // 
+            this.chPhone.Text = "전화번호";
+            this.chPhone.Width = 150;
+            // 
+            // chJob
+            // 
+            this.chJob.Text = "직업";
+            this.chJob.Width = 178;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 319);
+            this.Controls.Add(this.lvList);
+            this.Controls.Add(this.plGroup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "인명부";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.plGroup.ResumeLayout(false);
+            this.plGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plGroup;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblJob;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListView lvList;
+        private System.Windows.Forms.ColumnHeader chId;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chAge;
+        private System.Windows.Forms.ColumnHeader chPhone;
+        private System.Windows.Forms.ColumnHeader chJob;
+    }
+}
+
---
+++ [2021] Second_Semester/B_13/B_HumanList/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;    // OleDbConnection, OleDbCommand 클래스 등을 사용하기 위해 선언
+
+namespace B_HumanList
+{
+    public partial class Form1 : Form
+    {
+        private string StrSQL = @"Provider=Microsoft.ACE.OLEDB.16.0;Data Source=humaninfo.accdb;Mode=ReadWrite";    // DB 연결 문자열
+        private int num = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txtCheck() == true)
+            {
+                var conn = new OleDbConnection(StrSQL);
+                conn.Open();
+
+                string SQL = "INSERT INTO t_info(m_name, m_age, m_phone, m_job) VALUES('";
+
+                SQL += this.txtName.Text + "'," + Convert.ToInt32(this.txtAge.Text) + ", '" + this.txtPhone.Text + "', '" + this.txtJob.Text + "')";
+
+                var comm = new OleDbCommand(SQL, conn);
+                int i = comm.ExecuteNonQuery();
+                conn.Close();
+
+                if(i == 1)
+                {
+                    MessageBox.Show("정상적으로 데이터가 저장되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    lvList_DataSet_View();
+                    txtClear();
+                }
+                else
+                {
+                    MessageBox.Show("오류가 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool txtCheck()
+        {
+            if(this.txtName.Text == "")
+            {
+                this.txtName.Focus();
+                return false;
+            }
+            if (this.txtAge.Text == "")
+            {
+                this.txtAge.Focus();
+                return false;
+            }
+            if (this.txtPhone.Text == "")
+            {
+                this.txtPhone.Focus();
+                return false;
+            }
+            if (this.txtJob.Text == "")
+            {
+                this.txtJob.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lvList_OleDb_View();
+        }
+
+        private void lvList_OleDb_View()
+        {
+            this.lvList.Items.Clear();
+
+            var conn = new OleDbConnection(StrSQL);
+            conn.Open();
+
+            var comm = new OleDbCommand("SELECT * FROM t_info", conn);
+            var myRead = comm.ExecuteReader();
+
+            while(myRead.Read())
+            {
+                this.lvList.Items.Add(new ListViewItem(new string[] {myRead[0].ToString(), myRead[1].ToString(),
+                myRead[2].ToString(), myRead[3].ToString(), myRead[4].ToString()}));
+            }
+
+            myRead.Close();
+            conn.Close();
+        }
+
+        private void txtClear() // 입력란 공백 처리
+        {
+            this.txtName.Clear();
+            this.txtAge.Clear();
+            this.txtPhone.Clear();
+            this.txtJob.Clear();
+        }
+
+        private void lvList_Click(object sender, EventArgs e)
+        {
+            if(this.lvList.SelectedItems.Count > 0)
+            {
+                num = Convert.ToInt32(this.lvList.SelectedItems[0].SubItems[0].Text);
+
+                this.txtName.Text = this.lvList.SelectedItems[0].SubItems[1].Text;
+                this.txtAge.Text = this.lvList.SelectedItems[0].SubItems[2].Text;
+                this.txtPhone.Text = this.lvList.SelectedItems[0].SubItems[3].Text;
+                this.txtJob.Text = this.lvList.SelectedItems[0].SubItems[4].Text;
+            }
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            if(txtCheck() == true)
+            {
+                var conn = new OleDbConnection(StrSQL);
+                conn.Open();
+                string SQL = "UPDATE t_info SET m_name ='" + this.txtName.Text + "', m_age=" +
+                Convert.ToInt32(this.txtAge.Text);
+                SQL += ", m_phone='" + this.txtPhone.Text + "', m_job= '" + this.txtJob.Text + "'WHERE m_id = " + num + "";
+                var comm = new OleDbCommand(SQL, conn);
+                int i = comm.ExecuteNonQuery();
+
+                conn.Close();
+
+                if (i == 1)
+                {
+                    MessageBox.Show("정상적으로 데이터가 수정되었습니다.", "알림",  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lvList_DataSet_View();
+                    txtClear();
+                }
+                else
+                {
+                    MessageBox.Show("데이터가 수정되지 않았습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void lvList_DataSet_View()
+        {
+            this.lvList.Items.Clear();
+
+            var conn = new OleDbConnection(StrSQL);
+            conn.Open();
+
+            var OleAdapter = new OleDbDataAdapter("SELECT * FROM t_info", conn);
+
+            DataSet ds = new DataSet();
+            DataTable dt = ds.Tables.Add("dsTable");
+            OleAdapter.Fill(ds, "dsTable");
+
+            IEnumerable<DataRow> query = from Humaninfo in dt.AsEnumerable() select Humaninfo;
+
+            foreach (DataRow HumData in query)
+            {
+                var strArray = new String[] { HumData.Field<int>("m_id").ToString(), HumData.Field<string>("m_name"), HumData.Field<int>("m_age").ToString(),
+                            HumData.Field<string>("m_phone"), HumData.Field<string>("m_job")};
+
+                this.lvList.Items.Add(new ListViewItem(strArray));
+            }
+
+            conn.Close();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if(this.lvList.SelectedItems.Count > 0)
+            {
+                DialogResult dlr = MessageBox.Show("선택한 데이터를 삭제하시겠습니까?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (dlr)
+                {
+                    case DialogResult.Yes:
+                        var Conn = new OleDbConnection(StrSQL);
+                        Conn.Open();
+                        string Sql = "DELETE FROM t_info WHERE m_id =" + num + "";
+                        var Comm = new OleDbCommand(Sql, Conn);
+                        int i = Comm.ExecuteNonQuery();
+                        Conn.Close();
+
+                        if(i == 1)
+                        {
+                            MessageBox.Show("정상적으로 데이터가 삭제되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            lvList_DataSetRamda_View();
+                            txtClear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("데이터가 삭제되지 않았습니다.", "에러",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        break;
+
+                    case DialogResult.No:
+                        break;
+                }
+            }
+        }
+
+        private void lvList_DataSetRamda_View()
+        {
+            this.lvList.Items.Clear();
+            var conn = new OleDbConnection(StrSQL);
+            conn.Open();
+
+            var OleAdapter = new OleDbDataAdapter("SELECT * FROM t_info", conn);
+
+            DataSet ds = new DataSet();
+            DataTable dt = ds.Tables.Add("dsTable");
+
+            OleAdapter.Fill(ds, "dsTable");
+
+            var query = dt.AsEnumerable().
+            Select(HumanInfo => new
+            {
+                Id = HumanInfo.Field<int>("m_id").ToString(),
+                Name = HumanInfo.Field<string>("m_name"),
+                Age = HumanInfo.Field<int>("m_age").ToString(),
+                Phone = HumanInfo.Field<string>("m_phone"),
+                Job = HumanInfo.Field<string>("m_job"),
+            });
+
+            foreach (var HumData in query)
+            {
+                var strArray = new String[] { HumData.Id, HumData.Name, HumData.Age, HumData.Phone, HumData.Job };
+                this.lvList.Items.Add(new ListViewItem(strArray));
+            }
+            conn.Close();
+        }
+    }
+}
